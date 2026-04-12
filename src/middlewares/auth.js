7c35c8b08e65e10b72deb6787077e8181a61cf2c
Dianosse <antoine.lievre77@gmail.com>
2026-04-12 17:47:10 +0200

    refactor : meilleure mise en place du middleware d'auth avec vérification d'infos sur le compte de l'user présent dans le cookie

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,47 @@
-function protect(req, res, next) {
-    if (!req.session) {
+const userModel = require('../models/users');
+
+async function protect(req, res, next) {
+    try {
+        if (!req.session?.user) {
+            return res.status(401).json({
+                success: false,
+                error: 'Non authentifié'
+            });
+        }
+
+        const user = await userModel.findByPk(req.session.user.id);
+
+        if (!user) {
+            req.session.destroy(() => {});
+            return res.status(401).json({
+                success: false,
+                error: 'Session invalide'
+            });
+        }
+
+        if (user.ban) {
+            req.session.destroy(() => {});
+            return res.status(403).json({
+                success: false,
+                error: 'Compte banni'
+            });
+        }
+
+        req.user = {
+            id: user.id,
+            mail: user.mail,
+            pseudo: user.pseudo,
+            admin: user.admin
+        };
+
+        return next();
+    } catch (error) {
+        console.error(error);
         return res.status(500).json({
             success: false,
-            error: 'Session non initialisée'
+            error: 'Erreur serveur'
         });
     }
-
-    if (!req.session.user) {
-        return res.status(401).json({
-            success: false,
-            error: 'Non authentifié'
-        });
-    }
-
-    req.user = req.session.user;
-    return next();
 }
 
 module.exports = protect;
